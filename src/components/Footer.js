0c7c94b9889176bf0f3dd8394cf66c664fa0a2b5
Pavel <palllke2015@gmail.com>
2024-08-14 07:52:00 +0000

    123

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,41 +1,51 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect } from "react";
 import FooterTop from "./FooterTop";
-import footerBottom from "./Img/footerBottom.svg"
+import footerBottom from "./Img/footerBottom.svg";
 
 function Footer() {
-    const [isUp, setIsUp] = useState(false);
-    const [isRotate, setIsRotate] = useState(false)
+  const [isUp, setIsUp] = useState(false);
+  const [isRotate, setIsRotate] = useState(false);
 
-    useEffect(() => {
-        const element = document.getElementById('footerButton')
-        if (isRotate) {
-            element.style.animation = 'rotateTo 1s ease-in-out forwards';
-        } else {
-            element.style.animation = 'rotateFrom 1s ease-in-out forwards';
-        }
-    },[isRotate])
+  // useEffect(() => {
+  //     const element = document.getElementById('footerButton')
+  //     if (isRotate) {
+  //         element.style.animation = 'rotateTo 1s ease-in-out forwards';
+  //     } else {
+  //         element.style.animation = 'rotateFrom 1s ease-in-out forwards';
+  //     }
+  // },[isRotate])
 
-    useEffect(() => {
-        const element = document.getElementById('myBox');
-        if (isUp) {
-            element.style.animation = 'moveUp 1s ease-in-out forwards';
-        } else {
-            element.style.animation = 'moveDown 1s ease-in-out forwards';
-        }
-    }, [isUp]);
+  // useEffect(() => {
+  //     const element = document.getElementById('myBox');
+  //     if (isUp) {
+  //         element.style.animation = 'moveUp 1s ease-in-out forwards';
+  //     } else {
+  //         element.style.animation = 'moveDown 1s ease-in-out forwards';
+  //     }
+  // }, [isUp]);
 
-    const handleClick = () => {
-        setIsUp(!isUp);
-        setIsRotate(!isRotate)
-    };
+  const handleClick = () => {
+    setIsUp(!isUp);
+    setIsRotate(!isRotate);
+  };
 
-    return (<footer className="footer">
-        <div id="myBox" className="box"><FooterTop /></div>
-        <div className="footerBottom">
-            <button id='footerButton' onClick={handleClick} className="footerBottom--button"><img src={footerBottom} alt="" /></button>
-        </div>
+  return (
+    <footer className="footer">
+      <div id="myBox" className="box">
+        <FooterTop />
+      </div>
+      <div className="footerBottom">
+        <button
+          onClick={handleClick}
+          className={`footerBottom--button ${
+            isRotate ? "rotate-in" : "rotate-out"
+          }`}
+        >
+          <img src={footerBottom} alt="" />
+        </button>
+      </div>
     </footer>
-    );
+  );
 }
 
-export default Footer
+export default Footer;
