0550e880a760f6310510424d25a9b17d321a6791
cashok1029 <cashok1029@gmail.com>
2024-08-16 13:53:45 +0300

    fix functional

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,53 +1,56 @@
-import React, { useState, useEffect } from "react";
-import FooterTop from "./FooterTop";
+import React, { useState } from "react";
 import footerBottom from "./Img/footerBottom.svg";
+import logo from "./Img/logo.svg"
+import list from "./Img/list.svg"
+import email from "./Img/footerMail.svg"
+import phone from "./Img/footerPhone.svg"
+import social from "./Img/footerSocial.svg"
 
 function Footer() {
-  const [isUp, setIsUp] = useState(false);
-  const [isRotate, setIsRotate] = useState(false);
+    const [active, setActive] = useState(false);
 
 
-  // useEffect(() => {
-  //     const element = document.getElementById('footerButton')
-  //     if (isRotate) {
-  //         element.style.animation = 'rotateTo 1s ease-in-out forwards';
-  //     } else {
-  //         element.style.animation = 'rotateFrom 1s ease-in-out forwards';
-  //     }
-  // },[isRotate])
+    const handleClick = () => {
+        setActive(prev => !prev)
+    };
 
-
-  // useEffect(() => {
-  //     const element = document.getElementById('myBox');
-  //     if (isUp) {
-  //         element.style.animation = 'moveUp 1s ease-in-out forwards';
-  //     } else {
-  //         element.style.animation = 'moveDown 1s ease-in-out forwards';
-  //     }
-  // }, [isUp]);
-
-  const handleClick = () => {
-    setIsUp(!isUp);
-    setIsRotate(!isRotate);
-  };
-
-  return (
-    <footer className="footer">
-      <div id="myBox" className="box">
-        <FooterTop />
-      </div>
-      <div className="footerBottom">
-        <button
-          onClick={handleClick}
-          className={`footerBottom--button ${
-            isRotate ? "rotate-in" : "rotate-out"
-          }`}
-        >
-          <img src={footerBottom} alt="" />
-        </button>
-      </div>
-    </footer>
-  );
+    return (
+        <footer className="footer">
+            <div className={`footerTop ${active ? "active" : "inActive"}`}>
+                <img className="footerTop__logo" src={logo} alt="" />
+                <div className="footerTop__about">
+                    <div className="footerTop__about__element"><img className="footer__list--img" src={list} alt="" />О компании</div>
+                    <div className="footerTop__about__element"><img className="footer__list--img" src={list} alt="" />Сотрудничество</div>
+                    <div className="footerTop__about__element"><img className="footer__list--img" src={list} alt="" />Покупателям</div>
+                </div>
+                <div className="footerTop__about">
+                    <div className="footerTop__about__element"><img className="footer__list--img" src={list} alt="" />Где купить</div>
+                    <div className="footerTop__about__element"><img className="footer__list--img" src={list} alt="" />Сервисное обслуживание</div>
+                    <div className="footerTop__about__element"><img className="footer__list--img" src={list} alt="" />Контакты</div>
+                </div>
+                <div className="footerTop__questions">
+                    <div className="footerTop__questions--title">Есть вопросы?</div>
+                    <div className="footerTop__questions__element"><img src={email} alt="" />hello@orangemixers.ru</div>
+                    <div className="footerTop__questions__element"><img src={phone} alt="" />+380 (95) 43 23 45</div>
+                </div>
+                <div className="footerTop__social">
+                    <div className="footerTop__social--element">Мы в соцсетях</div>
+                    <img src={social} alt="" />
+                </div>
+                <div className="footerTop__bottom">
+                    <div className="footerTop__bottom--text">© 2019 Orange Mixers</div>
+                    <div className="footerTop__bottom--element">Разработано в bitberry.ru</div>
+                </div>
+            </div>
+            <div className="footerBottom">
+                <button
+                    onClick={handleClick}
+                    className={`footerBottom--button ${active ? "active" : "inActive"}`}>
+                    <img src={footerBottom} alt="" />
+                </button>
+            </div>
+        </footer>
+    );
 
 }
 
--- src/components/HeaderList.js
+++ src/components/HeaderList.js
@@ -1,90 +1,50 @@
-import React, { useEffect, useState } from "react";
+import React, { useState } from "react";
 import list from "../components/Img/list.svg"
-import AboutCompany from "./AboutCompany";
-import AboutGoods from "./AboutGoods";
-import Katalog from "./Katalog";
+import mixer from "../components/Img/mixer.svg"
+import furniture from "../components/Img/furniture.svg"
+import shower from "../components/Img/shower.svg"
+import fayas from "../components/Img/fayas.svg"
 
 
 function HeaderList() {
-    const [isListElementRotate, setIsListElementRotate] = useState(false)
-    const [isListElementRotateSecond, setIsListElementRotateSecond] = useState(false)
-    const [isListElementRotateThird, setIsListElementRotateThird] = useState(false)
-    const [isAboutCompanyVisible, setIsAboutCompanyVisible] = useState(false)
-    const [isAboutGoodsVisible, setIsAboutGoodsVisible] = useState(false)
-    const [isKatalogVisible, setIsKatalogVisible] = useState(false)
-
-    useEffect(() => {
-        const element = document.getElementById('list__img')
-        if (isListElementRotate) {
-            element.style.animation = 'rotateListTo 1s forwards';
-        } else {
-            element.style.animation = 'rotateListFrom 1s  forwards';
-        }
-    })
-    useEffect(() => {
-        const element = document.getElementById('list__imgSecond')
-        if (isListElementRotateSecond) {
-            element.style.animation = 'rotateListTo 1s forwards';
-        } else {
-            element.style.animation = 'rotateListFrom 1s  forwards';
-        }
-    })
-    useEffect(() => {
-        const element = document.getElementById('list__imgThird')
-        if (isListElementRotateThird) {
-            element.style.animation = 'rotateKatalogTo 1s forwards';
-        } else {
-            element.style.animation = 'rotateKatalogFrom 1s  forwards';
-        }
-    })
-
-    useEffect(() => {
-        const element = document.getElementById('aboutGoods')
-        if (isKatalogVisible) {
-            element.style.animation = 'visibleList 1s forwards';
-        } else {
-            element.style.animation = 'closeList 1s ease-in-out forwards';
-        }
-    })
-
-    useEffect(() => {
-        const element = document.getElementById('aboutCompany')
-        if (isAboutCompanyVisible) {
-            element.style.animation = 'visibleAbout 1s forwards';
-        } else {
-            element.style.animation = 'closeAbout ease-in-out 1s forwards';
-        }
-    })
-    useEffect(() => {
-        const element = document.getElementById('headerKatalog')
-        if (isAboutGoodsVisible) {
-            element.style.animation = 'visibleKatalog 1s forwards';
-        } else {
-            element.style.animation = 'closeKatalog ease-in-out 1s forwards';
-        }
-    })
+    const [activeCompany, setActiveCompany] = useState(false)
+    const [activeAboutCatalog, setActiveAboutCatalog] = useState(false)
+    const [activeKatalog, setActiveKatalog] = useState(false)
 
     const aboutCompanyClick = () => {
-        setIsAboutCompanyVisible(!isAboutCompanyVisible)
-        setIsListElementRotate(!isListElementRotate)
+        setActiveCompany(prev => !prev)
     }
 
     const aboutKatalogVisible = () => {
-        setIsKatalogVisible(!isKatalogVisible)
-        setIsListElementRotateSecond(!isListElementRotateSecond)
+        setActiveAboutCatalog(prev => !prev)
     }
 
     const katalog = () => {
-        setIsListElementRotateThird(!isListElementRotateThird)
-        setIsAboutGoodsVisible(!isAboutGoodsVisible)
+        setActiveKatalog(prev => !prev)
     }
 
     return (
         <div>
             <ul className="header__bottom--list">
-                <li className="header__bottom--list--element"><button onClick={katalog} className="header__bottom--list--element--button"><img id="list__imgThird" className="header__bottom--list--element--img" src={list} alt="" /></button><span className="header__bottom--list--element--text">Каталог товаров</span><Katalog /></li>
-                <li className="header__bottom--list--element"><button onClick={aboutCompanyClick} className="header__bottom--list--element--button"><img id="list__img" className="header__bottom--list--element--img" src={list} alt="" /></button><span className="header__bottom--list--element--text">О компании</span><AboutCompany /></li>
-                <li className="header__bottom--list--element"><button onClick={aboutKatalogVisible} className="header__bottom--list--element--button"><img id="list__imgSecond" className="header__bottom--list--element--img" src={list} alt="" /></button><span className="header__bottom--list--element--text">О продукции</span><AboutGoods /></li>
+                <li className="header__bottom--list--element"><button onClick={katalog} className="header__bottom--list--element--button"><img className={`header__bottom--list--element--img ${activeKatalog ? "activeKatalog" : "inActiveKatalog"}`} src={list} alt="" /></button><span className="header__bottom--list--element--text">Каталог товаров</span>
+                    <div className="parent__katalog">
+                        <div className={`katalog  ${activeKatalog ? "activeKatalog" : "inActiveKatalog"}`}>
+                            <div className="katalog__element"><img src={mixer} alt="" />Смесители</div>
+                            <div className="katalog__element"><img src={furniture} alt="" />Мебель</div>
+                            <div className="katalog__element"><img src={shower} alt="" />Душевая программа</div>
+                            <div className="katalog__element"><img src={fayas} alt="" />Фаянс</div>
+                        </div>
+                    </div></li>
+                <li className="header__bottom--list--element "><button onClick={aboutCompanyClick} className="header__bottom--list--element--button"><img className={`header__bottom--list--element--img ${activeCompany ? "activeAboutCompany" : "inActiveAboutCompany"}`} src={list} alt="" /></button><span className="header__bottom--list--element--text">О компании</span><div className="parent__aboutCompany">
+                    <div className={`aboutCompany ${activeCompany ? "activeAboutCompany" : "inActiveAboutCompany"}`}>
+                        <p>Orange - немецкая торговая марка. Главный офис и основное производство расположены в городе Бурбах в Германии. Также в городе Киев Киевской области находится наше собственное производство мебели для ванных комнат по чертежам и под чутким контролем сотрудников немецкой компании ORANGE mixers UG.</p>
+                    </div>
+                </div></li>
+                <li className="header__bottom--list--element"><button onClick={aboutKatalogVisible} className="header__bottom--list--element--button"><img  className={`header__bottom--list--element--img ${activeAboutCatalog ? "activeAboutCatalog" : "inActiveAboutCatalog"}`} src={list} alt="" /></button><span className="header__bottom--list--element--text">О продукции</span><div className="parent__aboutGoods">
+                    <div className={`aboutGoods ${activeAboutCatalog ? "activeAboutCatalog" : "inActiveAboutCatalog"}`}>
+                        <p>Orange Сантехника - это не просто цвет, это качество. Изготовленная из лучших материалов, наша сантехника прослужит вам долгие годы. Выбирайте стиль и наслаждайтесь комфортом.</p>
+                    </div>
+                </div></li>
             </ul>
         </div>
     )
